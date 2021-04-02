010a849cb547e9bd07c88f69049dc7f4e071c766
Jo <joannadawnmiller@gmail.com>
2021-04-02 13:15:20 -0400

    pass second test for get states route

--- spec/requests/get_states_spec.rb
+++ spec/requests/get_states_spec.rb
@@ -9,4 +9,8 @@ describe "get all states route", :type => :request do
     expect(JSON.parse(response.body).size).to eq(20)
   end
 
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
 end
