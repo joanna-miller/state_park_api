2d822f43947b7fdfecdda89c88900202877f7301
Jo <joannadawnmiller@gmail.com>
2021-04-02 13:21:00 -0400

    pass second test for post state route

--- spec/requests/post_state_spec.rb
+++ spec/requests/post_state_spec.rb
@@ -6,6 +6,10 @@ describe "post a state route", :type => :request do
     post '/states', params: { :name => 'Delaware' }
   end
 
+  it 'returns the state name' do
+    expect(JSON.parse(response.body)['name']).to eq('Delaware')
+  end
+
   it 'returns a created status' do
     expect(response).to have_http_status(:created)
   end
