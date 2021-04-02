088d5c020a3686d7229cf41795cd1158e35608eb
Jo <joannadawnmiller@gmail.com>
2021-04-02 14:28:46 -0400

    pass tests for post park route

--- spec/requests/get_states_spec.rb
+++ spec/requests/get_states_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe "get all states route", :type => :request do
-  let!(:states) { FactoryBot.create_list(:state, 20)}
+  let!(:states) { FactoryBot.create_list(:state, 10)}
   before { get '/states'}
 
   it 'returns all states' do
-    expect(JSON.parse(response.body).size).to eq(20)
+    expect(JSON.parse(response.body).size).to eq(10)
   end
 
   it 'returns status code 200' do
