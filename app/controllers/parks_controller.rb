64f802be0fc45ccbdf2631da6ecf52434a92e6d2
Jo <joannadawnmiller@gmail.com>
2021-04-02 14:17:03 -0400

    pass tests for get state park route

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -6,4 +6,20 @@ class ParksController < ApplicationController
     json_response(@parks)
   end
 
+  def show
+    @state = State.find(params[:state_id])
+    @park = Park.find(params[:id])
+    if @park.state_id == @state.id
+      json_response(@park)
+    end
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    json_response(@park, :created)
+  end
+
+  def park_params
+    params.permit(:name, :info, :park_type, :state_id)
+  end
 end
---
+++ spec/requests/get_park_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "get park route", :type => :request do
+
+  before do
+    @state = FactoryBot.create(:state)
+    park = Park.create({name: 'A Really Cool Park', info: 'A beautiful park great for hiking and fishing', park_type: 'State Park', state_id: @state.id})
+    get "/states/#{@state.id}/parks/#{park.id}"
+  end
+
+  it 'returns park' do
+    expect(JSON.parse(response.body)['name']).to eq('A Really Cool Park')
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'returns an error if park does not exist' do
+    get "/states/#{@state.id}/parks/50"
+    expect(JSON.parse(response.body)['message']).to eq("Couldn't find Park with 'id'=50")
+  end
+end
