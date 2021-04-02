4a9b300b270c88439a95822208e10e6d9fb1aecd
Jo <joannadawnmiller@gmail.com>
2021-04-02 18:36:21 -0400

    pass validate presence test for info

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,6 @@
 class Park < ApplicationRecord
   belongs_to :state
   validates :name, presence: true
+  validates :park_type, presence: true
+  validates :info, presence: true
 end
--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe Park do
   it { should belong_to(:state) }
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:park_type) }
+  it { should validate_presence_of(:info) }
 end
